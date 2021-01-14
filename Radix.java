0498224844700fb632105b7231cd4797b080805c
Elizabeth Stansberry <estansberry20@stuy.edu>
2021-01-13 23:15:40 -0700

    Done with testing

--- Radix.java
+++ Radix.java
@@ -1,12 +1,12 @@
 public class Radix{
   public static void main(String args[]){
-    MyLinkedList newlist = new MyLinkedList;
-    newlist.add("hello");
-    newlist.add("hey");
-    newlist.add("hi");
-    newlist.add("hola");
+    MyLinkedList newlist = new MyLinkedList();
   }
   public static int nth(int n, int col){
+    n = Math.abs(n);
+    if(col != 0 && n/(10^(col - 1)) < 1){
+      return(0);
+    }
     int newnum = 0;
     for(int i = 0; i <= col; i++){
       newnum = n % 10;
@@ -24,10 +24,54 @@ public class Radix{
       len += 1;
     }return(len);
   }
-  public static void merge(MyLinkedList original, MyLinkedList[]buckets){
+  public static void merge(SortableLinkedList original, SortableLinkedList[]buckets){
     for(int i = 0; i < buckets.length; i ++){
       original.extend(buckets[i]);
     }
   }
 
+  public static void radixSortSimple(SortableLinkedList data){
+    int largest = 0;
+    for(int i = 0; i < data.size(); i ++){
+      if(length(data.get(i)) > largest){
+        largest = length(data.get(i));
+      }
+    }SortableLinkedList[] bucket = new SortableLinkedList[10];
+    for(int i = 0; i < 10; i++){
+      bucket[i] = new SortableLinkedList();
+    }for(int i = 0; i < largest; i ++){
+      while(data.size() > 0){
+        int val = data.get(0);
+        int digit = nth(val, i);
+        bucket[digit].add(val);
+        data.remove(0);
+      }merge(data, bucket);
+    }
+  }
+
+  public static void radixSort(SortableLinkedList data){
+    int largest = 0;
+    for(int i = 0; i < data.size(); i ++){
+      if(length(data.get(i)) > largest){
+        largest = length(data.get(i));
+      }
+    }SortableLinkedList[] positives = new SortableLinkedList[10];
+    for(int i = 0; i < 10; i++){
+      positives[i] = new SortableLinkedList();
+    }SortableLinkedList[] negatives = new SortableLinkedList[10];
+    for(int i = 0; i < 10; i++){
+      negatives[i] = new SortableLinkedList();
+    }for(int i = 0; i < largest; i ++){
+      while(data.size() > 0){
+        int val = data.get(0);
+        int digit = nth(val, i);
+        if(val < 0){
+          negatives[9 - digit].add(val);
+        }else{
+          positives[digit].add(val);
+        }data.remove(0);
+      }merge(data, negatives);
+      merge(data, positives);
+    }
+  }
 }
